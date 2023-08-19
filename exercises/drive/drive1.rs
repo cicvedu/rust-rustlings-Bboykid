3623d5ef2605a432780150deee9864b6f73acbc6
Bboykid <872738753@qq.com>
2023-08-19 18:57:25 +0800

    100

--- exercises/drive/drive1.rs
+++ exercises/drive/drive1.rs
@@ -3,11 +3,17 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn modify_by_address(address: usize) {
     // `address` is a memory address, there is an u32 at that address. try modify
     // the u32's value to 0xAABBCCDD
+    // Convert the address back to a mutable reference of u32
+    let ptr = address as *mut u32;
+
+    // Use unsafe block to modify the value at the given address
+    unsafe {
+        *ptr = 0xAABBCCDD;
+    }
 }
 
 
