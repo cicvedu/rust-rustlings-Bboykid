87058f21ea07f880e89dc69069f893eab3a026a8
Bboykid <872738753@qq.com>
2023-08-19 18:50:23 +0800

    test git

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -3,8 +3,6 @@
 // This program spawns multiple threads that each run for at least 250ms, and
 // each thread returns how much time they took to complete. The program should
 // wait until all the spawned threads have finished and should collect their
-// return values into a vector.
-//
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 
 // I AM NOT DONE
